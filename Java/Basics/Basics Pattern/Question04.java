d738b9bce88658c9fa344ee008d7fa5c96dd6a4a
Nikhil Anand <nikhilanand2001bam@gmail.com>
2025-05-30 22:06:22 +0530

    updated solved probems

--- Java/Basics/Basics Pattern/Question04.java
+++ Java/Basics/Basics Pattern/Question04.java
@@ -5,11 +5,9 @@
 // * * * * *
 public class Question04 {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++)
-        {
-            for(int j=1;j<=5;j++)
-            {
-                System.out.print("*"+ " ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
