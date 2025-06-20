880d001bd003118cab2adcf664b33f0247c2ca38
Nikhil Anand <nikhilanand2001bam@gmail.com>
2025-06-20 22:15:49 +0530

    Practised Bubble,selection and insertion sort

---
+++ Java/Intermediate/Sorting/Question01.java
@@ -0,0 +1,31 @@
+public class Question01 {
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minpos]) {
+                    minpos = j;
+                }
+            }
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        System.out.println("Array Before Sorting :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Array after sorting : ");
+        selectionSort(arr);
+    }
+}
---
+++ Java/Intermediate/Sorting/Question02.java
@@ -0,0 +1,31 @@
+// Practise of Bubble sort 
+public class Question02 {
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+
+        System.out.println("Array Before Sorting : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        
+        System.out.println("Array After Sorting : ");
+        bubbleSort(arr);
+    }
+}
